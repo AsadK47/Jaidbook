916f397a89dbd582fba4d317b9764d36ec5fa84f
kiedunne <kie.dunne@gmail.com>
2018-12-21 11:30:27 +0000

    fixed home rspec error

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -6,12 +6,6 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content("Jaidbook Posts")
   end
 
-  scenario "Shows about" do
-    visit "/about"
-    expect(page).to have_content("About Us")
-    expect(page).to have_content("This is the about page")
-  end
-
   scenario "Adds new posts" do
     visit_homepage_sign_up_and_sign_in
     add_post
@@ -21,7 +15,7 @@ RSpec.feature "Timeline", type: :feature do
     visit_homepage_sign_up_and_sign_in
     click_link "Create Post"
     add_post
-    visit '/'
+    find('#home').click
     expect(page).to have_content("Some title")
     expect(page).to have_content("Some body")
   end
@@ -30,7 +24,7 @@ RSpec.feature "Timeline", type: :feature do
     visit_homepage_sign_up_and_sign_in
     click_link "Create Post"
     add_post
-    visit '/'
+    find('#home').click
     click_link "Read More"
     expect(page).to have_content("Some title")
     expect(page).to have_content("Some body")
@@ -40,7 +34,7 @@ RSpec.feature "Timeline", type: :feature do
     visit_homepage_sign_up_and_sign_in
     click_link "Create Post"
     add_post
-    visit '/'
+    find('#home').click
     click_link "Read More"
     click_link "Edit"
     fill_in "Title", with: "Other title"
@@ -54,7 +48,7 @@ RSpec.feature "Timeline", type: :feature do
     visit_homepage_sign_up_and_sign_in
     click_link "Create Post"
     add_post
-    visit '/'
+    find('#home').click
     click_link "Read More"
     click_link "Delete"
     expect(page).not_to have_content("Some title")
