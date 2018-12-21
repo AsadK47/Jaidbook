601293ab17e19a941b6bdf82ed3febafb951ba71
kiedunne <kie.dunne@gmail.com>
2018-12-21 12:10:57 +0000

    Profile tests passing

---
+++ spec/features/profile_displays_user_posts_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Profile", type: :feature do
+  scenario "Signed in users have a profile with their email " do
+    visit_homepage_sign_up_and_sign_in
+    find('#user-profile').click
+    expect(page).to have_content("test@test.com")
+  end
+
+  scenario "Profile shows posts" do
+    visit_homepage_sign_up_and_sign_in
+    add_post
+    find('#user-profile').click
+    expect(page).to have_content("Some title")
+    expect(page).to have_content("Some body")
+  end
+
+  scenario "Profile does not show other users posts" do
+    visit_homepage_sign_up_and_sign_in_2
+    add_post_2
+    find('#user-profile').click
+    expect(page).not_to have_content("Some title")
+    expect(page).not_to have_content("Some body")
+  end
+
+end
