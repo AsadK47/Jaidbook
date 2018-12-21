806cf4def5e22fdd95517c0f4dbf9307050a6bcb
Darryl Banks <darryl.banks@zombie9.com>
2018-12-21 15:37:49 +0000

    can delete comments and user stored in comment table

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,14 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create(merge_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
     redirect_to post_path(@post)
   end
 
@@ -10,6 +17,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def merge_params
+    comment_params.merge(user: current_user.email)
   end
 
 end
