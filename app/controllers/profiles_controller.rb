f1b5e88ba0ec699e51085fc57f8565b9d9b8ceb9
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2019-01-07 16:58:17 +0000

    user sign up redirects to the profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,13 @@
 class ProfilesController < ApplicationController
+
+  before_action :authenticate_user!, except: :index
+  before_action :find_post, except: [:index, :new, :create]
+
   def show
     @posts = Post.where(:user_id => current_user).order('posts.created_at DESC')
   end
+
+  def find_post
+  end
+
 end
