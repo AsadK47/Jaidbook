3367a4506bb38a4ba10b64a1de832449ac027b78
Darryl Banks <darryl.banks@zombie9.com>
2019-01-10 11:25:15 +0000

    fix foreign key violation bug for delete posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts
-  has_many :friendships
-  has_many :friends, through: :friendships
-  has_one_attached :avatar
+  has_many :posts, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
 end
