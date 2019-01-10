6e7417e6fbc8d297d8b2cddbfa9418872a4e95c3
Darryl Banks <darryl.banks@zombie9.com>
2019-01-10 16:15:19 +0000

    add Like functionality to posts

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'users#show', as: :profile
 
   resources :posts do
+    resources :likes
     resources :comments
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_135901) do
+ActiveRecord::Schema.define(version: 2019_01_10_144523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2019_01_09_135901) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", id: :serial, force: :cascade do |t|
     t.text "title"
     t.text "body"
@@ -91,4 +100,6 @@ ActiveRecord::Schema.define(version: 2019_01_09_135901) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
